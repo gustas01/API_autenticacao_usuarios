d89093665a2f150523b1be8bfe8e28bbaec240db
gustas01 <gustasboyevolution@hotmail.com>
2022-09-08 02:26:53 -0300

    adicionando métodos do CRUD de usuários no controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,14 @@ import User from '../models/User'
 let userController = {}
 
 userController.index = async (req, res) => {
-  res.send("olá do controler do user no indexxx")
+ try{
+  const users = await User.findAll()
+  res.json(users)
+ }catch(e){
+  res.status(400).json({
+    errors: e.errors.map(err => err.message)
+  })
+ }
 }
 
 
@@ -11,7 +18,7 @@ userController.create = async (req, res) => {
   try{
     const novoUser = await User.create(req.body)
     const {email, password} = novoUser
-    res.json({email, password})
+    return res.json({email, password})
   }catch(e){
     res.status(400).json({
       errors: e.errors.map(err => err.message)
@@ -21,17 +28,78 @@ userController.create = async (req, res) => {
 
 
 userController.read = async (req, res) => {
-  res.send("olá do controle no read")
+  try{
+    if(!req.params.id){
+      return res.status(400).json({errors: ["ID não enviado"]})
+    }
+
+    const user = await User.findByPk(req.params.id)
+
+    if(!user){
+      return res.status(404).json({errors: ["Usuário não existe"]})
+    }
+
+    return res.status(200).json({id: user.id, email: user.email})
+  }catch(e){
+    return res.status(400).json({
+      errors: e.errors.map(err => err.message)
+    })
+  }
 }
 
 
 userController.update = async (req, res) => {
-  res.send("olá do controle no update")
+  try{
+    console.log(req.params.id)
+    if(!req.params.id){
+      return res.status(404).json({errors: ["ID não enviado"]})
+    }
+
+    const user = await User.findByPk(req.params.id)
+
+    if(!user){
+      return res.status(404).json({errros: ["Usuário não encontrado"]})
+    }
+
+    const userUpdated = await user.update(req.body)
+    return res.status(200).json(userUpdated)
+
+  }catch(e){
+    return res.status(400).json({
+      errors: e.errors.map(err => err.message)
+    })
+  }
 }
 
 
 userController.delete = async (req, res) => {
-  res.send("olá do controle no delete")
+  try{
+    if(!req.params.id){
+      return res.status(400).json({errors: ["ID não enviado"]})
+    }
+
+    const user = await User.findByPk(req.params.id)
+
+    if(!user){
+      return res.status(404).json({errors: ["Usurário não encontrado"]})
+    }
+
+    //1ª forma de fazer
+    User.destroy({
+      where: {
+        id: req.params.id
+    }})
+
+    //2ª forma de fazer
+    // await user.destroy()
+
+    return res.status(200).json(user)
+  }catch(e){
+    console.log(e)
+    return res.status(200).json({
+      errors: e.errors.map(err => err.message)
+    })
+  }
 }
 
 
