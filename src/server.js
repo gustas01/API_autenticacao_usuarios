33443db0edc6950b289a61933fd1c5ec8662f9a0
gustas01 <gustasboyevolution@hotmail.com>
2022-09-14 15:13:05 -0300

    adicionando CORS

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import express from 'express'
 import helmet from 'helmet'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
 import homeRoutes from './routes/homeRoutes.js'
 import tokenRoutes from './routes/tokenRoutes.js'
 
+const whiteList = [
+  'https://localhost:3000'
+]
+
+const corsOption = {
+  origin: function(origin, callback){
+    if(whiteList.indexOf(origin) !== -1 || !origin)
+      callback(null, true)
+      else{
+        callback(new Error('Not allowed by CORS'))
+      }
+  }
+}
 const app = express()
 dotenv.config()
 
@@ -17,5 +31,6 @@ app.use('/', homeRoutes)
 
 
 app.use(helmet())
+app.use(cors(corsOption))
 
 app.listen(process.env.APP_PORT, () => console.log(`servidor executando na porta 3000 ${process.env.APP_URL}`))
